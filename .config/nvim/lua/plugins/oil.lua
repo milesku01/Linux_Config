1d6c7da90d5dd343ae528d377fcd8dbb2480e9f0
Miles Kuhn <mileskuhn5@hotmail.com>
2025-03-30 21:22:19 -0500

    Show hidden files with oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -11,6 +11,9 @@ return {
 
   config = function()
     require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
     }
   end,
